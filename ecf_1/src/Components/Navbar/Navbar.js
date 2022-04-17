e05108a3a97083b5321edbb7c15d511e0e6a2e1c
pierro7583 <93189698+pierro7583@users.noreply.github.com>
2022-04-17 17:06:40 +0200

    Integration Nav bar bootstrap

--- ecf_1/src/Components/Navbar/Navbar.js
+++ ecf_1/src/Components/Navbar/Navbar.js
@@ -3,13 +3,76 @@ import {Link} from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
 
-<Link to='/'>Acceuil</Link>
-<Link to='/Reservation'>Reservation</Link>
-<Link to='/Nouscontacter'>Nous contacter</Link>
-<Link to='/Sinscrire'>S'inscrire</Link>
-<Link to='/Seconnecter'>Se connecter</Link>
+
+
+
+
+
+<div className="container-fluid">
+    <Link to='/'>
+    <a className="navbar-brand" href="#">Acceuil</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    </Link>
+    
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <Link to='/Reservation'>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page">Reservation</a>
+        </li>
+        </Link>
+
+        <Link to='/Nouscontacter'>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page">Nous contacter</a>
+        </li>
+        </Link>
+
+        <Link to='/Sinscrire'>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page">S'inscrire</a>
+        </li>
+        </Link>
+       
+        <Link to='/Seconnecter'>
+
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page">Se connecter</a>
+        </li>
+
+        </Link>
+        
+        
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Rechercher" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Rechercher</button>
+      </form>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     </nav>
