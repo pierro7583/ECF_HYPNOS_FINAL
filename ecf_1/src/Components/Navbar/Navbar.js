4a2b163c35a9b2f317f386a0382a86da0902004c
pierro7583 <93189698+pierro7583@users.noreply.github.com>
2022-04-17 19:48:05 +0200

    Update Navbar.js

--- ecf_1/src/Components/Navbar/Navbar.js
+++ ecf_1/src/Components/Navbar/Navbar.js
@@ -22,6 +22,15 @@ export default function Navbar() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
     
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+
+      <Link to='/'>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page">Acceuil</a>
+        </li>
+        </Link>
+
+
       <Link to='/Reservation'>
         <li className="nav-item">
           <a className="nav-link active" aria-current="page">Reservation</a>
