772c9f70bf98de6e1ebd07f6c76099f2e3de4e7e
pierro7583 <93189698+pierro7583@users.noreply.github.com>
2022-04-17 19:39:29 +0200

    Ajout Margin article Acceuil + Carte reservation

--- ecf_1/src/Components/Acceuil/Acceuil.js
+++ ecf_1/src/Components/Acceuil/Acceuil.js
@@ -7,6 +7,49 @@ export default function Acceuil() {
     <div>
         <h1>Bienvenue chez Hypnos</h1>
  <img className="img-Acceuil" src='images/fond.jpg'></img>
+
+
+ <div className="container Acceuil_1">
+  <div className="row">
+    <div className="col">
+      <p>Lorem ipsum dolor sit amet,
+           consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna
+            aliqua.
+            </p>
+    </div>
+    
+    
+  </div>
+</div>
+
+
+
+ <div className="container Acceuil_2">
+  <div className="row">
+    <div className="col">
+      <p>Lorem ipsum dolor sit amet,
+           consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna
+            aliqua.
+            </p>
+    </div>
+    <div className="col">
+    <p>Lorem ipsum dolor sit amet,
+           consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna
+            aliqua.
+            </p>
+    </div>
+    <div className="col">
+    <p>Lorem ipsum dolor sit amet,
+           consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna
+            aliqua.
+            </p>
+    </div>
+  </div>
+</div>
         
     </div>
   )
--- ecf_1/src/Components/Reservation/Reservation.js
+++ ecf_1/src/Components/Reservation/Reservation.js
@@ -5,6 +5,80 @@ export default function Reservation() {
     <div>
 
         <h1>Reservation</h1>
+
+
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+  <div className="col">
+    <div className="card">
+      <img src="/images/biarritz.jpg" class="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card">
+      <img src="/images/chamonix-mont-blanc.jpg" class="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card">
+      <img src="/images/lille_home.jpg" class="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card">
+      <img src="/images/Nice.jpg" class="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+
+
+  <div className="col">
+    <div className="card">
+      <img src="/images/Marseille.jpg" class="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card">
+      <img src="/images/Plouescat.jpg" class="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+
+  <div className="col">
+    <div className="card">
+      <img src="/images/Strasbourg.jpg" class="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
     </div>
   )
 }
