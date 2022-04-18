252be6d058d5fd0253d4209553e23839db6ef9f9
pierro7583 <93189698+pierro7583@users.noreply.github.com>
2022-04-18 14:31:00 +0200

    Nouveau modal boot

--- ecf_1/src/Components/Reservation/Reservation.js
+++ ecf_1/src/Components/Reservation/Reservation.js
@@ -1,24 +1,19 @@
-import React , {useState} from 'react'
-import Modal from "./Modal";
-import "./Modal.css";
+import React  from 'react'
+
+
+
 
 export default function Reservation() {
-  const [modalOpen, setModalOpen] = useState(false);
+ 
+
+
+
 
   return (
-
-
-
-
-
 
     
     <div>
 
-
-
-
-      
 
         <h1>Réservation</h1>
 
@@ -31,8 +26,8 @@ export default function Reservation() {
         <h5 className="card-title">Biarritz</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
-      <button type="button" class="btn btn-primary openModalBtn" onClick={() => {setModalOpen(true); }}>Réserver</button>
-      {modalOpen && <Modal setOpenModal={setModalOpen} />}
+      <button type="button" class="btn btn-primary openModalBtn" data-bs-toggle="modal" data-bs-target="#Modal-Biarritz" >Réserver</button>
+    
     </div>
     
   </div>
@@ -45,7 +40,9 @@ export default function Reservation() {
         <h5 className="card-title">Chamonix</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
-      <button type="button" class="btn btn-primary" >Réserver</button>
+      <button type="button" class="btn btn-primary openModalBtn" data-bs-toggle="modal" data-bs-target="#Modal-Chamonix"   >Réserver</button>
+     
+      
     </div>
 
   </div>
@@ -58,7 +55,7 @@ export default function Reservation() {
         <h5 className="card-title">Lille</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
       </div>
-      <button type="button" class="btn btn-primary">Réserver</button>
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Modal-Lille" >Réserver</button>
     </div>
   </div>
 
@@ -73,7 +70,7 @@ export default function Reservation() {
         <h5 className="card-title">Nice</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
-      <button type="button" class="btn btn-primary">Réserver</button>
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Modal-Nice" >Réserver</button>
     </div>
   </div>
 
@@ -87,7 +84,7 @@ export default function Reservation() {
         <h5 className="card-title">Marseille</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
-      <button type="button" class="btn btn-primary">Réserver</button>
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Modal-Marseille">Réserver</button>
     </div>
   </div>
 
@@ -101,7 +98,7 @@ export default function Reservation() {
         <h5 className="card-title">Plouescat</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
-      <button type="button" class="btn btn-primary">Réserver</button>
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Modal-Plouescat">Réserver</button>
     </div>
   </div>
 
@@ -115,10 +112,158 @@ export default function Reservation() {
         <h5 className="card-title">Strasbourg</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
-      <button type="button" class="btn btn-primary">Réserver</button>
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Modal-Strasbourg">Réserver</button>
     </div>
   </div>
 </div>
+
+
+
+{/* <!-- Modal Biarritz --> */}
+<div class="modal fade" id="Modal-Biarritz" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Biarritz
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+{/* <!-- Modal Chamonix --> */}
+<div class="modal fade" id="Modal-Chamonix" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Chamonix
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+{/* <!-- Modal Lille --> */}
+<div class="modal fade" id="Modal-Lille" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       Lille
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+{/* <!-- Modal Nice --> */}
+<div class="modal fade" id="Modal-Nice" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       Nice
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+{/* <!-- Modal Marseille --> */}
+<div class="modal fade" id="Modal-Marseille" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       Marseille
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+{/* <!-- Modal Plouescat --> */}
+<div class="modal fade" id="Modal-Plouescat" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       Plouescat
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+{/* <!-- Modal Strasbourg --> */}
+<div class="modal fade" id="Modal-Strasbourg" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       Strasbourg
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
 
     </div>
   )
