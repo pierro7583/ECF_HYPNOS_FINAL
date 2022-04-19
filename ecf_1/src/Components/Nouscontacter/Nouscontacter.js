8e81f264d3e5d94272a5ae3577b594f90f32f1b8
pierro7583 <93189698+pierro7583@users.noreply.github.com>
2022-04-19 12:58:17 +0200

    Update Nouscontacter.js

--- ecf_1/src/Components/Nouscontacter/Nouscontacter.js
+++ ecf_1/src/Components/Nouscontacter/Nouscontacter.js
@@ -7,26 +7,40 @@ import React from "react";
 export default function Nouscontacter() {
   return (
     <div>
-        <h1>Nous contacter</h1>
-        <div class="mb-3 box">
-  <label for="exampleFormControlInput1" class="form-label">Nom</label>
-  <input type="text" class="form-control" id="Input_Nom" placeholder="Votre Nom"/>
-</div>
-<div class="mb-3 box">
-  <label for="exampleFormControlInput1" class="form-label">Prénom</label>
-  <input type="text" class="form-control" id="Input_Prénom" placeholder="Votre Prénom"/>
+      <h1>Nous contacter</h1>
+<div className="mb-3 box" >
+
+<select class="form-select" aria-label="Default select example">
+
+<option selected> Je souhaite poser une réclamation</option>
+<option value="1"> Je souhaite commander un service supplémentaire</option>
+<option value="2">Je souhaite en savoir plus sur une suite
+</option>
+<option value="3">J’ai un souci avec cette application</option>
+</select>
+
 </div>
 
-        <div class="mb-3 box">
-  <label for="exampleFormControlInput1" class="form-label">Adresse email</label>
-  <input type="email" class="form-control" id="Input_Email" placeholder="name@example.com"/>
-</div>
-<div class="mb-3 box">
-  <label for="exampleFormControlTextarea1" class="form-label">Message</label>
-  <textarea class="form-control" id="Input_Text" rows="3"></textarea>
-</div>
 
-<button type="button" class="btn btn-primary">Envoyé</button>
+      <div className="mb-3 box">
+        <label for="exampleFormControlInput1" class="form-label">Nom</label>
+        <input type="text" class="form-control" id="Input_Nom" placeholder="Votre Nom" />
+      </div>
+      <div className="mb-3 box">
+        <label for="exampleFormControlInput1" class="form-label">Prénom</label>
+        <input type="text" class="form-control" id="Input_Prénom" placeholder="Votre Prénom" />
+      </div>
+
+      <div className="mb-3 box">
+        <label for="exampleFormControlInput1" class="form-label">Adresse email</label>
+        <input type="email" class="form-control" id="Input_Email" placeholder="name@example.com" />
+      </div>
+      <div className="mb-3 box">
+        <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+        <textarea className="form-control" id="Input_Text" rows="3"></textarea>
+      </div>
+
+      <button type="button" class="btn btn-primary">Envoyé</button>
 
 
     </div>
